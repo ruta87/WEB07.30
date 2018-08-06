044aa95266a450278522304d88bb182c46b799e0
ruta87 <ruta.griskoniene@gmail.com>
2018-08-06 21:33:22 +0300

    pradeta baigiamasis

--- matematika/mat.js
+++ matematika/mat.js
@@ -5,3 +5,11 @@ while(i<b){
     i++;
     console.log(i);
 }
+document.getElementById("rezultatas"). innerHTML = 5+6;
+function f() {
+vardas=document.getElementById("vardas").value;
+amzius=document.getElementById("amzius").value;
+var dabar=(new Date()).getFullYear();
+document.getElementById('vardas_r').innerHTML= vardas;
+document.getElementById('amzius_r').innerHTML=(dabar-amzius)
+}
