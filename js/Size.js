c759a60129f91a3046e7700334b42988944f3402
ruta87 <ruta.griskoniene@gmail.com>
2018-08-09 21:34:15 +0300

    vel nuo pradziu...

--- js/Size.js
+++ js/Size.js
@@ -1,29 +1,20 @@
 var plotis;
 var ilgis;
 var storis;
+    Skaiciuoti = document.getElementById('Skaiciuoti').value;
 
-plotis=document.getElementById('plotis').value;
-var Skaiciuoti=function(){
-if(2070<=plotis<=2470){
-    Plotis=0;
-    document.getElementById('rezultatas').innerHTML = plotis + "pagaminti galime";
-}
-else{
-    document.getElementById('rezultatas').innerHTML = plotis + "pagaminti negalime";
-}}
-ilgis=document.getElementById('ilgis').value;
-var Skaiciuoti=function(){
-if(2000<=ilgis<=2800){
-    document.getElementById('rezultatas1').innerHTML = ilgis + "pagaminti galime";
-}
-else{
-    document.getElementById('rezultatas1').innerHTML = ilgis + "pagaminti negalime";
-}}
-storis=document.getElementById('storis').value;
-var Skaiciuoti=function(){
-if(10<=storis<=40){
-    document.getElementById('rezultatas2').innerHTML = storis + "pagaminti galime";
-}
-else{
-    document.getElementById('rezultatas2').innerHTML = storis + "pagaminti negalime";
-}}
+    if (2070 <= plotis && plotis <= 2470) {
+        document.getElementById('rezultatas').innerHTML = plotis + "pagaminti galime";
+    } else if (2000 <= ilgis && ilgis <= 2800){
+        document.getElementById('rezultatas1').innerHTML = ilgis + "pagaminti galime";
+        }
+        else if (10 <= storis && storis <= 40){
+        document.getElementById('rezultatas2').innerHTML = storis + "pagaminti galime"
+        }
+
+
+
+
+
+
+
