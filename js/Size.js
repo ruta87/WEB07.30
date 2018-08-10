d2c562fb5a97aa6ba9687c665c48507741e94ace
ruta87 <ruta.griskoniene@gmail.com>
2018-08-10 19:23:08 +0300

    paskutinis 0810

--- js/Size.js
+++ js/Size.js
@@ -1,7 +1,7 @@
-var plotis;
+/*var plotis;
 var ilgis;
-
-    Skaiciuoti = document.getElementById('Skaiciuoti').value;
+*/
+   /* Skaiciuoti = document.getElementById('Skaiciuoti').value;
 
     if (2070 <= plotis && plotis <= 2470) {
         document.getElementById('rezultatas').innerHTML = plotis + "pagaminti galime";
@@ -9,11 +9,18 @@ var ilgis;
         document.getElementById('rezultatas').innerHTML = plotis + "pagaminti negalime";
         }
 
+*/
 
+   function Skaiciuoti()
+   {
+       var plotis = document.getElementById('plotis').value;
+       var ilgis = document.getElementById('ilgis').value;
+       var storis = document.getElementById('storis').value;
 
-
-
-
-
-
+       if (2070 <= plotis && plotis <= 2470) {
+           document.getElementById('rezultatas').innerHTML = plotis + " pagaminti galime";
+       }else {
+           document.getElementById('rezultatas').innerHTML = plotis + " pagaminti negalime";
+       }
+   }
 
--- js/duomenu_uzkrovimas.js
+++ js/duomenu_uzkrovimas.js
@@ -1,4 +1,17 @@
 
     $.get("http://localhost:8080/rest/order/ordersba1", function(data, status){
-        $('#json_duomenys').html(data);
+        var result = "";
+        for(var i in data){
+            result +="<tr>\n" +
+                "            <th scope=\"fieldset\">"+data[i].id+"</th>\n" +
+                "            <td>"+data[i].position+"</td>\n" +
+                "            <td>12</td>\n" +
+                "            <td><input type=\"text\" name='' value='"+data[i].type+"'></td>\n" +
+                "            <td><input type=\"text\" name=''></td>\n" +
+                "            <td><input type=\"text\"></td>\n" +
+                "            <td><input type=\"text\"></td>\n" +
+                "        </tr>";
+        }
+        $('#results').html(result);
     });
+
