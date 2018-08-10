41a7bab64480498e8e0a99db41703e8f84f33e05
ruta87 <ruta.griskoniene@gmail.com>
2018-08-10 17:08:59 +0300

    link pabaigos...

--- js/Size.js
+++ js/Size.js
@@ -1,16 +1,15 @@
 var plotis;
 var ilgis;
-var storis;
+
     Skaiciuoti = document.getElementById('Skaiciuoti').value;
 
     if (2070 <= plotis && plotis <= 2470) {
         document.getElementById('rezultatas').innerHTML = plotis + "pagaminti galime";
-    } else if (2000 <= ilgis && ilgis <= 2800){
-        document.getElementById('rezultatas1').innerHTML = ilgis + "pagaminti galime";
+    } else {
+        document.getElementById('rezultatas').innerHTML = plotis + "pagaminti negalime";
         }
-        else if (10 <= storis && storis <= 40){
-        document.getElementById('rezultatas2').innerHTML = storis + "pagaminti galime"
-        }
+
+
 
 
 
